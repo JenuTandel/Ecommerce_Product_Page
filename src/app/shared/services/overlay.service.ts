fc43ad4d064fdbeff87ae1729e620035170b7d16
Jinal Tandel <jinal.tandel@1rivet.local>
2023-02-08 17:21:02 +0530

    feat: sub-cart

--- src/app/shared/services/overlay.service.ts
+++ src/app/shared/services/overlay.service.ts
@@ -1,9 +1,43 @@
-import { Injectable } from '@angular/core';
+import { ComponentRef, Injectable } from '@angular/core';
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OverlayService {
+  public closeDialog: Subject<boolean>;
+  public instance: any;
 
-  constructor() { }
+  constructor(private overlay: Overlay) {
+    this.closeDialog = new Subject();
+  }
+
+  openDialog(component: any): ComponentRef<any> {
+    const positionStrategy = this.overlay.position().global();
+
+    // Create the overlay with customizable options
+    const overlayRef = this.overlay.create({
+      positionStrategy,
+      backdropClass: 'overlay-backdrop',
+      hasBackdrop: true,
+      panelClass: 'overlay-panel',
+    });
+
+    const portal = new ComponentPortal(component);
+    this.instance = overlayRef.attach(portal);
+
+    // Close the dialog using backdropClick()
+    overlayRef.backdropClick().subscribe(() => {
+      overlayRef.detach();
+    });
+
+    this.closeDialog.subscribe((res) => {
+      if (res == true) {
+        overlayRef.detach();
+      }
+    });
+    return this.instance;
+  }
 }
